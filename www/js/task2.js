e027c22eeb61489f65c8d77251aa1b96ed923466
moran <moran678@163.com>
2016-12-01 23:47:10 +0800

    js task2

--- www/js/task2.js
+++ www/js/task2.js
@@ -5,35 +5,22 @@ $(document).ready(function(){
     var nKillerNum;
     var nPoliceNum;
     var nCivilianNum;
+    var NumArray=[];
+    var playerNum=$("#playerNum").val();
+    var nSlider=$("#sliderBlock").val();
     $("#sliderBlock").val(6);
     $('#sliderBlock').bind('input propertychange', function() {
         slider();
+       /* alert(playerNum)*/
     });
     $("#playerNum").change(function() {
         changeNum();
     });
     function changeNum(){
-      var playerNum=$("#playerNum").val();
         $('#sliderBlock').val(playerNum);
         if(playerNum<6){
             alert("最少6人才能玩游戏哦！")
-        }else if(playerNum<9){
-            nKillerNum=1;
-            nPoliceNum=1;
-            nCivilianNum=playerNum-nPoliceNum-nKillerNum;
-        }else if(playerNum<12){
-            nKillerNum=2;
-            nPoliceNum=1;
-            nCivilianNum=playerNum-nPoliceNum-nKillerNum;
-        }else if(playerNum<16){
-            nKillerNum=3;
-            nPoliceNum=2;
-            nCivilianNum=playerNum-nPoliceNum-nKillerNum;
-        }else if(playerNum<20){
-            nKillerNum=4;
-            nPoliceNum=2;
-            nCivilianNum=playerNum-nPoliceNum-nKillerNum;
-        }else{
+        }else if(n>20){
             alert("人数太多分两拨吧");
             $('#playerNum').css("color","#f00");
         }
@@ -69,6 +56,8 @@ $(document).ready(function(){
    function slider(){
        var nSlider=$("#sliderBlock").val();
        $("#playerNum").val(nSlider);
+       playerNum=nSlider;
+
    }
     /*点击设置*/
     $(".setBtn").click(function(){
@@ -76,13 +65,60 @@ $(document).ready(function(){
         var ListDoctor="<li class='doctor'>杀手</li>";
         var ListPolice="<li class='police'>警察</li>";
         var ListCivilian="<li class='civilian'>平民</li>";
-        $(".playerAllocation .List").html(ListCivilian);
+        $(".playerAllocation .List").empty();
         var Arr=["ListKiller","ListDoctor","ListCivilian"];
         nslider=$("#sliderBlock").val();
-        if(n>=6&&n<8){
-
+        for(i=0;i<nslider;i++){
+            $(".playerAllocation .List").append(ListCivilian);
         }
+        if(nslider<6){
+            alert("最少6人才能玩游戏哦！")
+        }else if(nslider<8){
+            nKillerNum=1;
+        }else if(nslider<11){
+            nKillerNum=2;
+        }else if(nslider<16){
+            nKillerNum=3;
+        }else if(nslider<20){
+            nKillerNum=4;
+        }else{
+            alert("人数太多分两拨吧");
+            $('#playerNum').css("color","#f00");
+        }
+
+
+
+         fetch(nKillerNum,nslider);
+
+            for(i=0;i<NumArray.length;i++){
+                $(".playerAllocation .List li").eq(NumArray[i]).attr("class","killer");
+                $(".playerAllocation .List li").eq(NumArray[i]).text("杀手");
+            }
+
+
     });
 
+    /*----------------随机产生m个（0-n）之间不同的数字----------------*/
+    function fetch(m,n)  {
+        if(m>n) {
+            document.write("你输入的不合理");
+        }else {
+            for(var i=0;i<m;i++) {
+                NumArray[i]=Math.round(n*Math.random());
+                for(var j=0;j<i;j++){
+                    if(NumArray[j]==NumArray[i]){
+                        while (1){
+                            NumArray[i]=parseInt(Math.random()*n);
+                            if(NumArray[i]!=NumArray[j]){
+                                j=-1;
+                                break;
+                            }
 
+                        }
+
+                    }
+                }
+            }
+        }
+    }
 });
