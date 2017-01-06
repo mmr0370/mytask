699f9404ed9608770ceaed1a4817d90447dc377c
moran <moran678@163.com>
2017-01-06 23:16:57 +0800

    task1 JS

--- www/js/task2.js
+++ www/js/task2.js
@@ -8,6 +8,7 @@ $(document).ready(function(){
     var NumArray=[];
     var playerNum=$("#playerNum").val();
     var nSlider=$("#sliderBlock").val();
+
     $("#sliderBlock").val(6);
     $('#sliderBlock').bind('input propertychange', function() {
         slider();
@@ -121,4 +122,36 @@ $(document).ready(function(){
             }
         }
     }
+    /*翻牌效果*/
+    var Default=function(){
+        var playerNum=1;
+        var playN=playerNum+1;
+        $()
+        $('.showIdentity .card-show .circle').text(playerNum);
+        $('.hideIdentity .card-hide .circle').text(playerNum);
+        $('.hideIdentity .transformBtn span').text(playerNum);
+        $('.showIdentity .transformBtn span').text(playN);
+    };
+    Default();
+   /* $('.hideIdentity .card-hide .circle').text(playerNum);*/
+    $('.hideIdentity .transformBtn').click(function(){
+        var opt=[{'width':0,'left':'50%'},{'width':'100%','left':0}];
+        $('.hideIdentity .transformBtn span').text($('.showIdentity .transformBtn span').text());
+        $('.hideIdentity .card-hide .circle').text($('.showIdentity .transformBtn span').text());
+        $('.hideIdentity').stop().animate(opt[0],150,function(){
+            $('.hideIdentity ').hide();
+            $('.showIdentity').show();
+            $('.showIdentity').animate(opt[1],150);
+        })
+    });
+    $('.showIdentity .transformBtn').click(function(){
+        $('.showIdentity .card-show .circle').text($('.showIdentity .transformBtn span').text());
+        $('.showIdentity .transformBtn span').text(parseInt($('.showIdentity .transformBtn span').text())+1);
+        var opt=[{'width':0,'left':'100%'},{'width':'100%','left':0}];
+        $('.showIdentity').stop().animate(opt[1],150,function() {
+            $('.showIdentity').hide();
+            $('.hideIdentity').show();
+            $('.hideIdentity').animate(opt[1], 150);
+        })
+    });
 });
